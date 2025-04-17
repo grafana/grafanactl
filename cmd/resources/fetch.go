8c993133d768feb115bd5e8cb773e5d62927c931
Kévin Gomez <kevin.gomez@grafana.com>
2025-04-17 23:50:03 +0200

    Bootstrap a `resources edit` command

--- cmd/resources/fetch.go
+++ cmd/resources/fetch.go
@@ -3,14 +3,16 @@ package resources
 import (
 	"context"
 
+	"github.com/grafana/grafanactl/cmd/fail"
 	"github.com/grafana/grafanactl/internal/config"
 	"github.com/grafana/grafanactl/internal/resources"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type fetchRequest struct {
-	Config      config.NamespacedRESTConfig
-	StopOnError bool
+	Config             config.NamespacedRESTConfig
+	StopOnError        bool
+	ExpectSingleTarget bool
 }
 
 type fetchResponse struct {
@@ -27,6 +29,13 @@ func fetchResources(ctx context.Context, opts fetchRequest, args []string) (*fet
 	pull, err := resources.NewPuller(ctx, opts.Config)
 	if err != nil {
 		return nil, err
+	}
+
+	if opts.ExpectSingleTarget && !sels.IsSingleTarget() {
+		return nil, fail.DetailedError{
+			Summary: "Invalid resource selector",
+			Details: "Expected a resource selector targeting a single resource. Example: dashboard/some-dashboard",
+		}
 	}
 
 	res := fetchResponse{
