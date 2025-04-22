d6ea16b4f43ba31ffff6d91440ed97abfd4137c2
Kévin Gomez <kevin.gomez@grafana.com>
2025-04-22 23:12:05 +0200

    Add a "resource validate" command

--- cmd/resources/command.go
+++ cmd/resources/command.go
@@ -24,6 +24,7 @@ TODO: more information.
 	cmd.AddCommand(pullCmd(configOpts))
 	cmd.AddCommand(pushCmd(configOpts))
 	cmd.AddCommand(serveCmd(configOpts))
+	cmd.AddCommand(validateCmd(configOpts))
 
 	return cmd
 }
