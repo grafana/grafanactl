grafana/grafanactl

commit cef9efc88b3430798ab8eadc11f21ab9b04a2c27

Improve API error handling (#128)
### What

This change updates the API client with better error parsing logic,
which produces more detailed API errors.

### Why

Default API error logging is not verbose enough and sometimes doesn't
produce enough information for debugging those errors.

e.g. when using invalid config for Grafana Cloud (using org ID instead
of stack ID):

Before:
```
❯ bin/grafanactl --context=broken resources get
WARN Could not pull resources err=unknown cmd=GetAll:playlists.v0alpha1.playlist.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:folders.v1beta1.folder.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:securevalues.v1beta1.secret.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:systems.v1alpha1.servicemodel.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:keepers.v1beta1.secret.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:relationdefinitions.v1alpha1.gamma.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:relationtypes.v1alpha1.gamma.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:resources.v1alpha1.servicemodel.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:relations.v1alpha1.gamma.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:components.v1alpha1.servicemodel.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:dashboards.v1beta1.dashboard.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:entitydefinitions.v1alpha1.gamma.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:sandbox-settings.v0alpha1.sandboxsettings.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:checks.v0alpha1.advisor.grafana.app
WARN Could not pull resources err=unknown cmd=GetAll:appo11yconfigs.v1alpha1.productactivation.ext.grafana.com
WARN Could not pull resources err=unknown cmd=GetAll:groups.v1alpha1.servicemodel.ext.grafana.com

internal/resources/dynamic/errors.go
@@ -1,0 +1,44 @@
+package dynamic
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// APIError is an error that wraps a Kubernetes API status.
+// It formats the error message in a more readable format
+// (since Kubernetes natively will only log the message, which could sometimes be simply "unknown").
+type APIError struct {
+	Status metav1.Status
+}
+
+// Error implements the error interface.
+// It logs the code & reason in addition to the message,
+// which could be simply "unknown" in some cases.
+func (e APIError) Error() string {
+	return fmt.Sprintf("%d %s: %s", e.Status.Code, string(e.Status.Reason), e.Status.Message)
+}
+
+// ParseStatusError parses a Kubernetes API status from an error.
+func ParseStatusError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if status, ok := err.(apierrors.APIStatus); ok || errors.As(err, &status) {
+		return APIError{status.Status()}
+	}
+
+	return APIError{
+		Status: metav1.Status{
+			Status:  metav1.StatusFailure,
+			Reason:  metav1.StatusReasonUnknown,
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		},
+	}
+}

internal/resources/dynamic/namespaced_client.go
@@ -39,7 +39,8 @@ func NewNamespacedClient(namespace string, client dynamic.Interface) *Namespaced
 func (c *NamespacedClient) List(
 	ctx context.Context, desc resources.Descriptor, opts metav1.ListOptions,
 ) (*unstructured.UnstructuredList, error) {
-	return c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).List(ctx, opts)
+	res, err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).List(ctx, opts)
+	return res, ParseStatusError(err)
 }
 
 // GetMultiple gets multiple resources from the server.
@@ -54,7 +55,7 @@ func (c *NamespacedClient) GetMultiple(
 ) ([]unstructured.Unstructured, error) {
 	res, err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).List(ctx, opts)
 	if err != nil {
-		return nil, err
+		return nil, ParseStatusError(err)
 	}
 
 	filtered := make([]unstructured.Unstructured, 0, len(res.Items))
@@ -73,33 +74,38 @@ func (c *NamespacedClient) GetMultiple(
 func (c *NamespacedClient) Get(
 	ctx context.Context, desc resources.Descriptor, name string, opts metav1.GetOptions,
 ) (*unstructured.Unstructured, error) {
-	return c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Get(ctx, name, opts)
+	res, err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Get(ctx, name, opts)
+	return res, ParseStatusError(err)
 }
 
 // Create creates a resource on the server.
 func (c *NamespacedClient) Create(
 	ctx context.Context, desc resources.Descriptor, obj *unstructured.Unstructured, opts metav1.CreateOptions,
 ) (*unstructured.Unstructured, error) {
-	return c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Create(ctx, obj, opts)
+	res, err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Create(ctx, obj, opts)
+	return res, ParseStatusError(err)
 }
 
 // Update updates a resource on the server.
 func (c *NamespacedClient) Update(
 	ctx context.Context, desc resources.Descriptor, obj *unstructured.Unstructured, opts metav1.UpdateOptions,
 ) (*unstructured.Unstructured, error) {
-	return c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Update(ctx, obj, opts)
+	res, err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Update(ctx, obj, opts)
+	return res, ParseStatusError(err)
 }
 
 // Delete deletes a resource on the server.
 func (c *NamespacedClient) Delete(
 	ctx context.Context, desc resources.Descriptor, name string, opts metav1.DeleteOptions,
 ) error {
-	return c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Delete(ctx, name, opts)
+	err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Delete(ctx, name, opts)
+	return ParseStatusError(err)
 }
 
 // Apply applies a resource on the server.
 func (c *NamespacedClient) Apply(
 	ctx context.Context, desc resources.Descriptor, name string, obj *unstructured.Unstructured, opts metav1.ApplyOptions,
 ) (*unstructured.Unstructured, error) {
-	return c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Apply(ctx, name, obj, opts)
+	res, err := c.client.Resource(desc.GroupVersionResource()).Namespace(c.namespace).Apply(ctx, name, obj, opts)
+	return res, ParseStatusError(err)
 }

commit b0c54f86a65fd5965da2d30d07c09a02946567fb

Fix API errors interface compliance (#139)
### What

This PR addresses API error interface compliance which was broken in
#128. This led to the problem that API errors were not identified
properly as `NotFound` or `AlreadyExists`, making `grafanactl push` not
work as expected.

### Why

To make sure that `grafanactl push` can properly create objects which
don't exist or overwrite existing objects.

---------

Signed-off-by: Igor Suleymanov <igor.suleymanov@grafana.com>

cmd/grafanactl/root/command.go
@@ -6,11 +6,13 @@ import (
 	"path"
 
 	"github.com/fatih/color"
+	"github.com/go-logr/logr"
 	"github.com/grafana/grafana-app-sdk/logging"
 	"github.com/grafana/grafanactl/cmd/grafanactl/config"
 	"github.com/grafana/grafanactl/cmd/grafanactl/resources"
 	"github.com/grafana/grafanactl/internal/logs"
 	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
 )
 
 func Command(version string) *cobra.Command {
@@ -29,13 +31,20 @@ func Command(version string) *cobra.Command {
 
 			logLevel := new(slog.LevelVar)
 			logLevel.Set(slog.LevelWarn)
-			logger := logging.NewSLogLogger(logs.NewHandler(os.Stderr, &logs.Options{
-				Level: logLevel,
-			}))
-
 			// Multiplying the number of occurrences of the `-v` flag by 4 (gap between log levels in slog)
 			// allows us to increase the logger's verbosity.
 			logLevel.Set(logLevel.Level() - slog.Level(min(verbosity, 3)*4))
+
+			logHandler := logs.NewHandler(os.Stderr, &logs.Options{
+				Level: logLevel,
+			})
+			logger := logging.NewSLogLogger(logHandler)
+
+			// Also set klog logger (used by k8s/client-go).
+			klog.SetLoggerWithOptions(
+				logr.FromSlogHandler(logHandler),
+				klog.ContextualLogger(true),
+			)
 
 			cmd.SetContext(logging.Context(cmd.Context(), logger))
 		},

internal/resources/dynamic/errors.go
@@ -9,18 +9,25 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+var _ apierrors.APIStatus = (*APIError)(nil)
+
 // APIError is an error that wraps a Kubernetes API status.
 // It formats the error message in a more readable format
 // (since Kubernetes natively will only log the message, which could sometimes be simply "unknown").
 type APIError struct {
-	Status metav1.Status
+	status metav1.Status
 }
 
 // Error implements the error interface.
 // It logs the code & reason in addition to the message,
 // which could be simply "unknown" in some cases.
 func (e APIError) Error() string {
-	return fmt.Sprintf("%d %s: %s", e.Status.Code, string(e.Status.Reason), e.Status.Message)
+	return fmt.Sprintf("%d %s: %s", e.status.Code, string(e.status.Reason), e.status.Message)
+}
+
+// Status implements the apierrors.APIStatus interface.
+func (e APIError) Status() metav1.Status {
+	return e.status
 }
 
 // ParseStatusError parses a Kubernetes API status from an error.
@@ -34,7 +41,7 @@ func ParseStatusError(err error) error {
 	}
 
 	return APIError{
-		Status: metav1.Status{
+		status: metav1.Status{
 			Status:  metav1.StatusFailure,
 			Reason:  metav1.StatusReasonUnknown,
 			Code:    http.StatusInternalServerError,

cmd/grafanactl/config/command_test.go
@@ -288,10 +288,12 @@ current-context: prod
 	testCase.Run(t)
 }
 
-func Test_ViewCommand_withEnvironmentVariablesAndNoConfig(t *testing.T) {
+func Test_ViewCommand_withEnvironmentVariablesAndEmptyConfig(t *testing.T) {
+	configFile := testutils.CreateTempFile(t, "contexts:")
+
 	testCase := testutils.CommandTestCase{
 		Cmd:     config.Command(),
-		Command: []string{"view", "--minify", "--raw"},
+		Command: []string{"view", "--config", configFile, "--minify", "--raw"},
 		Assertions: []testutils.CommandAssertion{
 			testutils.CommandSuccess(),
 			testutils.CommandOutputEquals(`contexts: